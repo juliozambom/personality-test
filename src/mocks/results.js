ed917b5e0e2eea09eabbfda20a2d2b71d0f2821f
Julio Zambom <work.juliozambom@gmail.com>
2022-06-07 23:00:59 -0300

    chore: finishing mocks

--- src/mocks/results.js
+++ src/mocks/results.js
@@ -1,33 +1,29 @@
 const results = [
-    {
-        name: "Carine Medeiros",
-        imageUrl: "",
-        textTitle: "",
-        text: ""
-    },
-    {
-        name: "Pedrinho Chavito 😊",
-        imageUrl: "https://i.imgur.com/PHrRmwG.jpg",
-        text: "Um ser que se ama demais, inteligente, dedicado e que faz de tudo para conseguir o que quer. Odeia admitir que está errado (irritante isso, but patience), tentando defender seu ponto de vista a todo custo o que provoca muitas vezes pequenos atritos com quem está ao seu redor. Mas, é uma pessoa engraçada, quer ver seus amigos felizes e é muito fiel a eles. Seu jeito de demonstrar carinho é diferenciado, as vezes nos xingos e as vezes nos elogios, porém, todos gostam."
-    },
-    {
-        name: "Catherine",
-        imageUrl: "",
-        textTitle: "",
-        text: ""
-    },
-    {
-        name: "Giovanna",
-        imageUrl: "",
-        textTitle: "Parabéns!! Você se parece mais com a Giovanna 🤩",
-        text: "Uma pessoa com uma presença incrível e que sempre eleva o astral de todos ao seu redor. Que preza pela sua família e amigos, e os quer sempre bem, estando presente nas horas de alegria e tristeza para ajuda-los e apoia-los, independente do que aconteça. Além de ser focada e dedicada no que lhe é importante, mas sem se privar dos momentos de diversão e distração, aproveitando-os ao máximo, pois afinal só se vive uma vez."
-    },
-    {
-        name: "Stephany",
-        imageUrl: "",
-        textTitle: "",
-        text: ""
-    }
-]
+  {
+    name: "Carine Medeiros",
+    imageUrl: "https://i.imgur.com/lxi3CIu.jpg",
+    text: "Ela é incrível, isso significa que você também é pois fez escolhas que te levaram a esse resultado, é sensível as vezes, mas tem um perfil forte por suportar quaisquer situações que já passou, é uma pessoa divertida e se esforça muito para conseguir aquilo que deseja, tem uma presença incrível, faz bem a todos que estão ao seu redor e adora uma fofoquinha e comentar as vergonhas alheias ao seu redor, ah, e ama KITKAT mais do que o normal.",
+  },
+  {
+    name: "Pedrinho Chavito 😊",
+    imageUrl: "https://i.imgur.com/PHrRmwG.jpg",
+    text: "Um ser que se ama demais, inteligente, dedicado e que faz de tudo para conseguir o que quer. Odeia admitir que está errado (irritante isso, but patience), tentando defender seu ponto de vista a todo custo o que provoca muitas vezes pequenos atritos com quem está ao seu redor. Mas, é uma pessoa engraçada, quer ver seus amigos felizes e é muito fiel a eles. Seu jeito de demonstrar carinho é diferenciado, as vezes nos xingos e as vezes nos elogios, porém, todos gostam.",
+  },
+  {
+    name: "Catherine",
+    imageUrl: "https://i.imgur.com/yUj8XKc.jpg",
+    text: "Uma pessoa esforçada, inteligente, carinhosa, que gosta de aprender coisas novas e se interessar por vários assuntos. Uma ótima pessoa para se conversar sobre planos e sobre o dia-a-dia, definidamente seus amigos confiam em você cegamente e você tem grande postura de maturidade e confiança, se tornando alguém interessante de conversar sem medo de se sentir julgado.",
+  },
+  {
+    name: "Giovanna",
+    imageUrl: "https://i.imgur.com/yzobJZn.jpg",
+    text: "Uma pessoa com uma presença incrível e que sempre eleva o astral de todos ao seu redor. Que preza pela sua família e amigos, e os quer sempre bem, estando presente nas horas de alegria e tristeza para ajuda-los e apoia-los, independente do que aconteça. Além de ser focada e dedicada no que lhe é importante, mas sem se privar dos momentos de diversão e distração, aproveitando-os ao máximo, pois afinal só se vive uma vez.",
+  },
+  {
+    name: "Stephany",
+    imageUrl: "https://i.imgur.com/XeJ88qP.jpg",
+    text: "Você é uma pessoa focada, inteligente, organizada e alegre. Provavelmente você é a pessoa que seus amigos querem ficar perto nos momentos bons e ruins. Sempre quando precisam de um conselho ou de conversar sobre assuntos aleatórios da vida procuram você. Por ser muito companheirx provavelmente você carrega fardos de muitas pessoas, não se deixe sobrecarregar por frustrações do dia a dia, tire seu próprio tempo para cuidar de si e faça aquilo que te deixa feliz. Pode ter certeza que em muitos lugares há pessoas que te admiram e querem sua amizade e seu bem estar!!!",
+  },
+];
 
 export default results;
